bd338b8ab78af76f19d182142ad0dc4e411d4f56
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-05-11 20:29:38 -0700

    Clean up bootstrap:
    Stick to booleans on init flag
    Sets up a proper user agent string
    Delays command registration until after WP has loaded

--- command.php
+++ command.php
@@ -4,6 +4,7 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
+// Temporary - needed until soter-core is ready to go up on Packagist.
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
@@ -12,22 +13,28 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 if ( ! function_exists( '_soter_command_init' ) ) {
 	function _soter_command_init() {
 		// This file is being loaded twice so let's keep track of initialization.
-		static $soter_command_initialized = null;
+		static $soter_command_initialized = false;
 
 		if ( ! $soter_command_initialized ) {
-			$http = new Soter_Core\WP_Http_Client( '@todo' );
+			$ua_parts = [
+				sprintf( '%s (%s)', get_bloginfo( 'name' ), get_home_url() ),
+				'Soter CLI',
+				'v0.1.0',
+				'https://github.com/ssnepenthe/soter-command',
+			];
+			$http = new Soter_Core\WP_Http_Client( implode( ' | ', $ua_parts ) );
 			$cache = new Soter_Core\WP_Transient_Cache( 'soter_command' );
 			$client = new Soter_Core\Api_Client( $http, $cache );
 			$checker = new Soter_Core\Checker( $client );
 
-			WP_CLI::add_command(
-				'soter',
-				new Soter_Command\Soter_Command( $checker )
-			);
+			$command = new Soter_Command\Soter_Command( $checker );
+
+			WP_CLI::add_command( 'soter', $command );
 
 			$soter_command_initialized = true;
 		}
 	}
-}
 
-_soter_command_init();
+	// Delay command registration so we can use get_bloginfo(), get_home_url().
+	WP_CLI::add_hook( 'after_wp_load', '_soter_command_init' );
+}
